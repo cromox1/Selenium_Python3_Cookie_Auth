caaee628fd728ebf90f1312515be46e167a63ae2
Rosli Talib <xixa01@yahoo.co.uk>
2020-06-12 13:27:24 +0100

    2020.06.12 enhance features of test2_pixitmedia* no1

--- UnitTests/test2_pixitmedia_hoover_around_downloadfile.py
+++ UnitTests/test2_pixitmedia_hoover_around_downloadfile.py
@@ -16,7 +16,7 @@ class TestHooverPixitMedia(unittest.TestCase):
     def setUpClass(cls):
         print('\n--- > setUpClass\n')
 
-    def setUp(self, browser='brave'):
+    def setUp(self, browser='chrome'):
         print('\n--- > setUp\n')
         # self.driver = webdriver.Firefox()
         if browser=='brave':
@@ -63,6 +63,7 @@ class TestHooverPixitMedia(unittest.TestCase):
         driver.find_element_by_name('q').send_keys("pixitmedia" + Keys.ENTER)
         list_pixitmedia = driver.find_elements_by_xpath("//*[contains(text(),'Pixit Media')]")
         self.assertGreaterEqual(len(list_pixitmedia), 1)
+
         # OLD code - not working anymore
         # for element in list_pixitmedia:
         #     if element.text == "https://www.pixitmedia.com/":
@@ -74,6 +75,7 @@ class TestHooverPixitMedia(unittest.TestCase):
         print('PIXITMEDIA URL = ' + str(basepixitmediaurl))
         elements_pixstor = driver.find_elements_by_xpath("//*[contains(text(),'PixStor')]")
         self.assertGreaterEqual(len(elements_pixstor), 1)
+
         element_pixstor = elements_pixstor[0]
         hoover(driver).move_to_element(element_pixstor).perform()
         features_el = driver.find_element_by_xpath("//*[contains(text(),'Features')]")
@@ -88,8 +90,11 @@ class TestHooverPixitMedia(unittest.TestCase):
         resc_el = driver.find_element_by_xpath("//*[contains(text(),'Resources')]")
         hoover(driver).move_to_element(resc_el).perform()
         resc_el.click()
-
-        driver.find_elements_by_xpath("//*[contains(text(),'Data Sheets')]")[0].click()
+        if driver.find_elements_by_xpath("//*[contains(text(),'Data Sheets')]"):
+            driver.execute_script("window.scrollTo(0, " + str(self.displayheight()[2]) + ");")
+            driver.find_elements_by_xpath("//*[contains(text(),'Data Sheets')]")[0].click()
+        else:
+            driver.find_elements_by_xpath("//*[contains(text(),'Data Sheets')]")[0].click()
         driver.execute_script("window.scrollTo(0, " + str(self.displayheight()[1]) + ");")
         sleep(2)
         pixstor_overview = driver.find_elements_by_xpath("//*[contains(text(),'PixStor Overview')]")
@@ -98,18 +103,19 @@ class TestHooverPixitMedia(unittest.TestCase):
             print('Element text = ' + str(pixstor_overview[0].text))
             pixstor_overview[0].click()
             # View Datasheet - PDF file
+            if driver.find_element_by_xpath("//*[contains(text(),'DOWNLOAD')]"):
+                driver.execute_script("window.scrollTo(0, " + str(self.displayheight()[2]) + ");")
             pdffileurl = driver.find_element_by_xpath("//*[contains(text(),'DOWNLOAD')]").get_attribute('href')
             print('PDF FILE URL = ' + str(pdffileurl))
             # validate PDF file
             wget.download(pdffileurl, out=self.tmpfilename)
             file1 = open(self.tmpfilename, "rb")
             chkfile = fleep.get(file1.read(128))
-            print('TYPE = ' + str(chkfile.type))
+            # print('TYPE = ' + str(chkfile.type))
             print('EXTN = ' + str(chkfile.extension))
             print('MIME = ' + str(chkfile.mime))
+            self.assertIn('pdf', str(chkfile.mime))
             file1.close()
-            # self.assertGreaterEqual(len(listintrsect), 1)
-
         else:
             print('PixStor Overview PDF NOT FOUND')
 
@@ -161,15 +167,17 @@ class TestHooverPixitMedia(unittest.TestCase):
         hoover(driver).move_to_element(element_message).perform()
         element_message.clear()
         current2 = strftime("%Y-%m-%d %H:%M:%S")
-        message1 = "This is a test for Jez \nToday is " + str(current2) + \
+        message1 = "This is a test for Jez \n\nToday is " + str(current2) + \
                    '\nExact testing time : From ' + str(current1) + ' to ' + str(current2) + \
-                   "\nThis Earth of Mankind (Bumi Manusia) -> is it Jez?? I don't think so. Look at you!!"
+                   "\nThis Earth of Mankind (Bumi Manusia) -> is it Jez?? " \
+                   "\nI don't think so. Look at the inconsiderate you!!"
         print('MESSAGE :\n' + str(message1))
         element_message.send_keys(message1)
         # Half page scroll down
         driver.execute_script("window.scrollTo(0, " + str(self.displayheight()[1]) + ");")
         driver.find_element_by_name('input_8.1').click()
         element_send = driver.find_element_by_xpath("//*[@value='Send']")
+        self.assertIsNotNone(element_send)
         hoover(driver).move_to_element(element_send).perform()
         element_send.click()
 
@@ -200,8 +208,9 @@ class TestHooverPixitMedia(unittest.TestCase):
         if last_height <= 1800:
             last_height = 1850
         half_height = int(0.5 * last_height)
-        print('HEIGHT = ' + str(last_height) + ' / 0.5HEIGHT = ' + str(half_height))
-        return last_height,half_height
+        quater_height = int(0.1 * last_height)
+        print('HEIGHT = ' + str(last_height) + ' / 0.5HEIGHT = ' + str(half_height) + ' / 0.1HEIGHT = ' + str(quater_height))
+        return last_height,half_height,quater_height
 
 if __name__ == "__main__":
     unittest.main()
